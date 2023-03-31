7e3a6c862c5a95a9f9ac83658282627d278d77de
basil <basil.rommens@student.uantwerpen.be>
2023-03-31 20:14:37 +0200

    added imputation

--- src/clean.py
+++ src/clean.py
@@ -1,6 +1,7 @@
 from sklearn.impute import KNNImputer
 import pandas as pd
 
+from src.impute import DataFrameImputer
 from src.read import read_parquet
 
 
@@ -18,15 +19,4 @@ if __name__ == '__main__':
     fname = 'existing-customers.parquet'
     df = read_parquet(path + fname)
     df = remove_rowid(df)
-    # before imputing the null values, we need to convert the categorical
-    # columns to numerical columns through one-hot encoding
-    df = pd.get_dummies(df, dummy_na=True)
-    for col in df.columns:
-        if 'workclass' in col:
-            print(col)
-            print(df[col].isnull().sum())
-
-    imputer = KNNImputer(n_neighbors=2)
-    df = imputer.fit_transform(df)
-    df = pd.DataFrame(df)
-    print(df)
+    df = DataFrameImputer().fit_transform(df)
---
+++ src/impute.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.base import TransformerMixin
+
+from src.exploration import show_null_vals
+from src.read import read_parquet
+
+
+# https://stackoverflow.com/questions/25239958/impute-categorical-missing-values-in-scikit-learn
+# 31/03/2022
+class DataFrameImputer(TransformerMixin):
+
+    def __init__(self):
+        """Impute missing values.
+
+        Columns of dtype object are imputed with the most frequent value
+        in column.
+
+        Columns of other types are imputed with mean of column.
+
+        """
+
+    def fit(self, X, y=None):
+        self.fill = pd.Series([X[c].value_counts().index[0]
+                               if X[c].dtype == np.dtype('O') else X[c].mean()
+                               for c in X],
+                              index=X.columns)
+
+        return self
+
+    def transform(self, X, y=None):
+        return X.fillna(self.fill)
+
+
+if __name__ == '__main__':
+    path = 'data/'
+    fname = 'existing-customers.parquet'
+    df = read_parquet(path + fname)
+    dft = DataFrameImputer().fit_transform(df)
+    show_null_vals(dft)  # no output expected
